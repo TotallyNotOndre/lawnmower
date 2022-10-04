0617c693e41dcd7f46471715c8691b5403466d67
TotalyNotOndre <40639199+TotalyNotOndre@users.noreply.github.com>
2022-10-04 23:21:23 +0300

    Move welcome message to config

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -1,7 +1,11 @@
 import { GuildMember } from "discord.js";
 import Logger from "../util/Logger";
+import getConfig from '../util/config';
 const c = new Logger("guildMemberAdd");
 
 export default async function run(member: GuildMember) {
     c.trail(`${member.user.username}#${member.user.discriminator} joined the server.`);
+	
+	const config = await getConfig();
+	member.user.send(`Hi ${member.user}, welcome to Grasscutters!\n` + config.welcome_message);
 }
